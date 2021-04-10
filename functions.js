0ae37ab79064057b99f852a41a77a1230c189d14
CMauro139 <68304362+CMauro139@users.noreply.github.com>
2021-04-09 22:09:13 -0400

    Update functions.js
    Added function - grab input values, turn strings into numbers grab elements calculations append with $ and show the bill info.

--- functions.js
+++ functions.js
@@ -1,1 +1,30 @@
+function calcTip(){
+  
+  //Grab the input values
+  let calcTip= document.getElementById("tipForm").value;
+  let billAmount= document.getElementById("billInput").value;
+  let billCard - document.getELementById("bill-card");
 
+  //turn strings into numbers
+   let tipAmountNumber = parseFloat(tipAmount);
+   let billAmountNumber = parsetFloat(billAmount);
+  
+  //grab elemenents
+  let bill = document.getElementById('bill-p');
+  let tip = document.getElementById('tip-p');
+  let total = document.getElementById('total-p');
+
+  // calculations
+  let totalAmount = (tipAmountNumber * billAMountNumber);
+  let totalBill = totalAmount + billAmountNumber;
+  
+  //append content
+  tip.innerHTML = "$" + (totalAmount).toFixed(2);
+  bill.innerHTML = "$" + billAmountNumnber.toFixed(2);
+ total.innerHTML = "$" + totalBill.toFixed(2);
+  
+  // show the bill info
+  billCard.style.display = "block";
+
+
+}
