4f3ed32788f2dcc354d3ef8baf11736b7f562677
CMauro139 <68304362+CMauro139@users.noreply.github.com>
2021-04-09 22:38:46 -0400

    Update script.js
    c&p file to see changes

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 function calcTip(){
-  
-  //Grab the input values
-  let tipAmount= document.getElementById("tipForm").value;
-  let billAmount= document.getElementById("billInput").value;
-  let billCard - document.getELementById("bill-card");
 
-  //turn strings into numbers
-   let tipAmountNumber = parseFloat(tipAmount);
-   let billAmountNumber = parsetFloat(billAmount);
-  
-  //grab elemenents
-  let bill = document.getElementById('bill-p');
-  let tip = document.getElementById('tip-p');
-  let total = document.getElementById('total-p');
+    // grab the input values
+    let tipAmount= document.getElementById("tipForm").value;
+    let billAmount = document.getElementById("billInput").value;
+    let billCard = document.getElementById("bill-card");
 
-  // calculations
-  let totalAmount = (tipAmountNumber * billAMountNumber);
-  let totalBill = totalAmount + billAmountNumber;
-  
-  //append content
-  tip.innerHTML = "$" + (totalAmount).toFixed(2);
-  bill.innerHTML = "$" + billAmountNumnber.toFixed(2);
- total.innerHTML = "$" + totalBill.toFixed(2);
-  
-  // show the bill info
-  billCard.style.display = "block";
+    //turn strings into numbers
+    let tipAmountNumber = parseFloat(tipAmount);
+    let billAmountNumber = parseFloat(billAmount);
+    
+    //grab elements
+    let bill = document.getElementById('bill-p');
+    let tip = document.getElementById('tip-p');
+    let total = document.getElementById('total-p');
 
+    // calculations
+    let totalAmount = (tipAmountNumber * billAmountNumber);
+    let totalBill = totalAmount + billAmountNumber;
 
+    //append content
+    tip.innerHTML = "$" + (totalAmount).toFixed(2);
+    bill.innerHTML =  "$" + billAmountNumber.toFixed(2);
+    total.innerHTML = "$" + totalBill.toFixed(2);
+    
+    // show the bill info 
+    billCard.style.display = "block";
+    
 }
